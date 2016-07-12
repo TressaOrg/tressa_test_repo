002586aee77d081750fe97af6eafc8b89ef41602
Graham St-Laurent <gstlaurent@gmail.com>
2016-07-11 23:43:36 -0700

    Created ADDED assertions.

---
+++ basic.c
@@ -0,0 +1,77 @@
+#include <assert.h>
+
+/*This is just a header
+ * It is included to ensure that
+ * there is enough
+ * room
+ * in this function
+ * so that nothing is left
+ * to the fact that
+ * the beginnign of the file
+ * is so close to
+ * any assertions.
+ * Let's see if if
+ * it works.
+ */
+
+void main() {
+    assert(a);
+    assert(to_delete);
+    assert(c);
+
+    assert(to_change);
+
+    assert(d);
+    assert(to_change_surrounded);
+    assert(f);
+
+    assert(to_change == b ||
+            (c != d &&
+             e == f));
+    assert(a==b ||
+            (c != d &&
+             e == no_change));
+    assert(a == b ||
+            (c != to_change &&
+             e == f));
+
+    big_assert_bob(bad);
+    improper(assert(good));
+    assert ( z );
+    assert 
+        (
+         x
+        );
+
+}
+
+/*This is just more comment.
+ * It is included to ensure that
+ * there is enough
+ * room
+ * in this function
+ * so that nothing is left
+ * to the fact that
+ * the beginnign of the file
+ * is so close to
+ * any assertions.
+ * Let's see if if
+ * it works.
+ */
+
+assert(outside);
+
+
+/*This is just more comment again!
+ * It is included to ensure that
+ * there is enough
+ * room
+ * in this function
+ * so that nothing is left
+ * to the fact that
+ * the beginnign of the file
+ * is so close to
+ * any assertions.
+ * Let's see if if
+ * it works.
+ * */
---
+++ comments.c
@@ -0,0 +1,68 @@
+#include <stdio.c>
+#include <stdlib.c>
+#include <assert.h>
+
+
+/* No asserts in the following should be included:
+ * Testing assert() in a comment sentence.
+ * Testing an assert without parens in a comment.
+ *
+   We aren't including the opening * here, but the assert() yes.
+   Now we have no stars whatsover, but we do have assert().
+   And here's just the assert okay.
+*/
+void comments(int good, int bad) {
+    assert(good==1);
+    // assert(bad==1);
+    
+    /* assert(bad==2); */
+
+    /* assert(
+        bad == 3
+       ); */ assert(good == 2);
+
+    /* bad==4 */ assert(good == 3);
+
+    assert(good /* bad==10 */ == 4);
+
+    assert(good // bad==11 
+            == 5);
+
+    assert(good ==
+                    /*
+                      bad==12
+                   */
+            6);
+
+    /* bad==13
+     * assert(maybe==1);
+     */
+
+
+    /*
+        assert(bad==5);
+    */
+
+
+    // Strings are similar to comments in this case
+    char *text = "assert(bad==6)";
+    char *t= "assert(bad==7)"; assert(good==7);
+
+    assert(post_comment_changes);  // comment
+    assert(mid_comment_changes /*comment*/ == 1);  
+    assert(comment_added == 1);
+
+    // This is about spaces, but that's similar to comment effects.
+    assert(good == not);
+
+}
+
+assert(good==9);
+
+#if 0
+void ignorable() {
+    assert(bad==8)
+}
+
+assert(bad==9);
+#endif
