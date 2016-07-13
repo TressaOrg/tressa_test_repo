e0972c0723ab6dee574b8a59974e2a9b69af3ef5
Graham St-Laurent <gstlaurent@gmail.com>
2016-07-12 20:05:07 -0700

    Added file for checking too many lines or multiple asserts per line.

---
+++ lines.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <assert.h>
+
+void function() {
+
+    // too long
+    assert(a == 1 && too_many_lines ||
+           b == 2   ||
+           c == 3   ||
+           d == 4   ||
+           e == 5   ||
+           f == 6   ||
+           g == 7   ||
+           h == 8   ||
+           i == 9   ||
+           j == 10  ||
+           k == 11  ||
+           l == 12  ||
+           m == 13  ||
+           n == 14  ||
+           o == 15  ||
+           p == 16);
+
+    assert(
+            good==1
+          );
+
+    // multiple asserts per line
+
+    assert(no_change1); assert(no_change2);
+    assert(to_change==1); assert(to_change==2);
+    assert(no_change3); assert(to_change==3);
+    assert(to_change==4); assert(no_change4);
+
+    assert(no_change5); assert(to_delete1);
+    assert(to_delete2); assert(no_change6); 
+    assert(to_delete3); assert(to_change==5); 
+
+    /*assert(to_add1);*/assert(to_change==6); 
+    /*assert(to_add2);*/assert(no_change7); 
+    assert(to_change==7); /*assert(to_add2);*/
+    assert(no_change8); /*assert(to_add3);*/
+
+    assert(a); assert(b); assert(c); assert(d); assert(e); assert(f); assert(g);
+
+
+}
+
